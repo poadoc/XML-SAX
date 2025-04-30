9b56e04cef2bec37412933ce2ae2d138fd1c40de
Ernest Zarichnyi <ernest.zarichnyi.pe@gmail.com>
2025-04-30 14:29:09 +0300

    REL-4333 Apply patch from Oracle to fix CPAN RT#20126
    Applying unchanged patches found in Oracle's perl-XML-SAX RPM:
    - Removing patented files: xmltest.xml and 16large.t
    - Applying the patch that fixes CPAN RT#20126
    
    Patented files need to be removed because they aren't allowed to
    be re-distributed; second change will fix RT#20126 bug.

--- lib/XML/SAX/PurePerl.pm
+++ lib/XML/SAX/PurePerl.pm
@@ -595,22 +595,23 @@ sub Comment {
     
     my $data = $reader->data(4);
     if ($data =~ /^<!--/) {
-        $reader->move_along(4);
+        $reader->move_along(4); # skip comment start
+        $data = $reader->data;
+        while ($data !~ m!-->!) {
+            my $n = $reader->read_more;
+             $self->parser_error("End of data seen while looking for close comment marker", $reader)
+                 unless $n;
+            $data = $reader->data;
+        }
+
         my $comment_str = '';
-        while (1) {
-            my $data = $reader->data;
-            $self->parser_error("End of data seen while looking for close comment marker", $reader)
-                unless length($data);
-            if ($data =~ /^(.*?)-->/s) {
-                $comment_str .= $1;
-                $self->parser_error("Invalid comment (dash)", $reader) if $comment_str =~ /-$/;
-                $reader->move_along(length($1) + 3);
-                last;
-            }
-            else {
-                $comment_str .= $data;
-                $reader->move_along(length($data));
-            }
+        if ($data =~ /^(.*?)-->/s) {
+            $comment_str = $1;
+            $self->parser_error("Invalid comment (dash)", $reader) if $comment_str =~ /-$/;
+            $reader->move_along(length($1) + 3);
+        }
+        else {
+            return 0;
         }
         
         $self->comment({ Data => $comment_str });
